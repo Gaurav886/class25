4b42755472b263799174d665d452af2b2ad373a0
Gaurav886 <82867960+Gaurav886@users.noreply.github.com>
2021-06-21 13:58:57 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,14 @@
+
+  
+class Box extends BaseClass {
+  constructor(x,y,width,hight){
+    super(x,y,width,hight);
+    this.image = loadImage("sprites/wood1.png");
+  }
+
+ // display() {
+   // this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    //super.display();
+ // }
+}
